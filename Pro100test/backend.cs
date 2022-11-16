5ca1277e37e86562f7703b30da6daee4c051b573
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2022-11-16 09:17:36 -0700

    Merge Check

--- Pro100test/backend.cs
+++ Pro100test/backend.cs
@@ -3,12 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Pro100test
 {
     internal class backend{
 
+        /*public string[] getDatabase(){
+
+        string[] database = new string[1000];
+
+        private static readonly HttpClient client = new HttpClient();
+
         string call = "https://id.twitch.tv/oauth2/token/client_id=ukinm8oq8njkab73c59c1b0vpmv69h/client_secret=9rprk08hlt1ci4ysb41wey5rvx7i1b/grant_type=client_credentials";
+
+        var values = new Dictionary<string, string>{{ "thing1", "hello" },{ "thing2", "world" }};
+
+        var content = new FormUrlEncodedContent(values);
+
+        var response = await client.PostAsync(call, content);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
+
+        return database;*/
+
+        }
+
+        /*POST https://api.igdb.com/v4/games
+        
+        Client-ID: abcdefg12345
+        
+        Authorization: Bearer access12345token
+        
+        fields*;*/
 
     }
 
