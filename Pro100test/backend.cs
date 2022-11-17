dc80367cf5e4b50a366947e21b9ceca6ec2b2215
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2022-11-17 14:55:56 -0700

    did some backend stuff with carter and added a wpf so we can have a good looking program

--- Pro100test/backend.cs
+++ Pro100test/backend.cs
@@ -4,41 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using SteamKit2.CDN;
+using SteamKit2.Internal;
 
 namespace Pro100test
 {
-    internal class backend{
+    internal class backend
+    {
 
-        /*public string[] getDatabase(){
+        public string[] getDatabase()
+        {
 
-        string[] database = new string[1000];
+            string[] database = new string[1000];
 
-        private static readonly HttpClient client = new HttpClient();
+            private HttpClient client = new HttpClient();
 
-        string call = "https://id.twitch.tv/oauth2/token/client_id=ukinm8oq8njkab73c59c1b0vpmv69h/client_secret=9rprk08hlt1ci4ysb41wey5rvx7i1b/grant_type=client_credentials";
+            KeyValuePair<string, string> clientID = new KeyValuePair<string, string>("client_id", "ukinm8oq8njkab73c59c1b0vpmv69h");
+        
+            KeyValuePair<string, string> clientSecret = new KeyValuePair<string, string>("client_secret", "rsx7v0kzkpzo5ejwffh3wufpnhz2hv");
+        
+            KeyValuePair<string, string> grantType = new KeyValuePair<string, string>("grant_type", "client_credentials");
 
-        var values = new Dictionary<string, string>{{ "thing1", "hello" },{ "thing2", "world" }};
+            client.BaseAddress("https://id.twitch.tv/oauth2/token");
+            client.DefaultRequestHeaders.Add(clientID.Key, clientID.Value);
+            client.DefaultRequestHeaders.Add(clientSecret.Key, clientSecret.Value);
+            client.DefaultRequestHeaders.Add(grantType.Key, grantType.Value);
 
-        var content = new FormUrlEncodedContent(values);
+            client.body();
 
-        var response = await client.PostAsync(call, content);
+            
+        //var response = await client.PostAsync(call, content);
 
-        var responseString = await response.Content.ReadAsStringAsync();
+        //var responseString = await response.Content.ReadAsStringAsync();
 
-        var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
+        //var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
 
-        return database;*/
+            //return database;
 
         }
 
-        /*POST https://api.igdb.com/v4/games
-        
-        Client-ID: abcdefg12345
-        
-        Authorization: Bearer access12345token
-        
-        fields*;*/
+    /*POST https://api.igdb.com/v4/games
 
-    }
+    Client-ID: abcdefg12345
+
+    Authorization: Bearer access12345token
+
+    fields*;*/
+
+}
 
 }
