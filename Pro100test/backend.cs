87417b594b77ffaf7f0f609d0f6cd737b2b6ba6f
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2022-11-18 08:19:10 -0700

    Merge Check

--- Pro100test/backend.cs
+++ Pro100test/backend.cs
@@ -5,17 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 
-namespace Pro100test
-{
-    internal class backend{
+namespace Pro100test{
+
+    internal class Backend {
+
+        var call = "https://id.twitch.tv/oauth2/token";
+
+
+        public void GetLibrary() {
+
+            HttpClient client = new HttpClient();
+
+             //"//=/=";
+
+            HttpRequestMessage requestu = new HttpRequestMessage(HttpMethod.Get,"https://id.twitch.tv/oauth2/token");
+
+            requestu.Headers.Add("client_id", "ukinm8oq8njkab73c59c1b0vpmv69h");
+            
+            requestu.Headers.Add("client_secret", "9rprk08hlt1ci4ysb41wey5rvx7i1b");          
+            
+            requestu.Headers.Add("grant_type", "client_credentials");          
+
+            //client.BaseAddress(call);
+
+        }
+
+
+    }
+
+    
 
         /*public string[] getDatabase(){
 
         string[] database = new string[1000];
 
-        private static readonly HttpClient client = new HttpClient();
+        
 
-        string call = "https://id.twitch.tv/oauth2/token/client_id=ukinm8oq8njkab73c59c1b0vpmv69h/client_secret=9rprk08hlt1ci4ysb41wey5rvx7i1b/grant_type=client_credentials";
+        
 
         var values = new Dictionary<string, string>{{ "thing1", "hello" },{ "thing2", "world" }};
 
@@ -29,7 +55,7 @@ namespace Pro100test
 
         return database;*/
 
-        }
+        
 
         /*POST https://api.igdb.com/v4/games
         
@@ -38,7 +64,5 @@ namespace Pro100test
         Authorization: Bearer access12345token
         
         fields*;*/
-
-    }
 
 }
